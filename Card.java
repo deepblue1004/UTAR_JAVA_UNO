9cca68821ecd005b1c9b334370f547f21513f6db
LEE YEN LONG <leeyenlong1@gmail.com>
2018-01-23 09:10:02 +0800

    Code update

--- Card.java
+++ Card.java
@@ -1,13 +1,21 @@
 //Card.java
 
 public class Card {
-	private enum fColor {
+	private enum eColor {
 		RED, GREEN, BLUE, YELLOW, WILD;
 	}
-	private enum fSymbol {
+	private enum eSymbol {
 		ZERO, ONE, TWO, THREE, FOUR, FIVE,
 		SIX, SEVEN, EIGHT, NINE, 
 		SKIP, REVERSE, WILD, DRAW_2, WILD_DRAW_4;
+	}
+	
+	private eColor fColor;
+	private eSymbol fSymbol;
+	
+	public Card(eColor aColor, eSymbol aSymbol) {
+		fColor = aColor;
+		fSymbol = aSymbol;	
 	}
 	
 	public void action () {
--- GameLogic.java
+++ GameLogic.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+
 //GameLogic.java
 
 public class GameLogic {
+	private Player[] fPlayer;
+	private ArrayList<Card> fDeck;
+	private ArrayList<Card> fPile;
+	
+	public Player getPlayer(int aIndex) {
+		return fPlayer[aIndex];
+	}
+	
+	public GameLogic(int aNumPlayer) {
+		suffleCard(fDeck);
+	}
+	
+	public static ArrayList<Card> createCard() {
+		ArrayList<Card> result = new ArrayList<Card>();
+		Card tempCard;
+		for(int i = 0; i < 56; i++) {
+			tempCard = new Card();
+			result.add(tempCard);
+		}
+		
+		return result;
+	}
+	public static void suffleCard(ArrayList<Card> aDeck) {
+		Card tempCard;
+		
+	}
 
 }
